97678ad9b8503d4c6cf647aa0443283d0d9a4039
sara hoxhaj <12242040@univie.ac.at>
2026-08-12 10:31:34 +0200

    all three vis and tasks

--- src/components/TaskView.vue
+++ src/components/TaskView.vue
@@ -10,11 +10,12 @@
                         <!-- <img src="@/assets/3.png" alt="Image 3" class="w-[750px] mt-20 ml-5"> -->
                     </div>
 
-                    <div class="max-w-2xl">
-                        <p class="text-gray-900 dark:text-white">
+                    <div class="max-w-2xl mr-5">
+                        <p class="text-gray-900 dark:text-white mb-5">
                             Below are three tasks that ask you to read specific information from the given
                             visualization.
                         </p><br>
+                        <!-- <p> THESE ARE FOR THE FIRST VIS </p> -->
                         <div class="text-left">
                             <p><b>1.</b> According to the chart, approximately what percentage of 15- to 24-year-olds in
                                 Germany
@@ -63,11 +64,120 @@
                         <div class="text-left mt-10">
                             <p><b>3.</b> If you had to summarize this chart in one sentence, what would you say?</p>
                             <textarea id="message" rows="1" v-model="visMessage"
+                                class="mt-2 required dark:text-black rounded-lg bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body"
+                                placeholder="Write your thoughts here..."></textarea>
+                        </div> 
+                        <!-- <p> HERE FINISHES THE FIRST VIS </p> -->
+
+
+
+
+                        <!-- <p> THESE ARE FOR THE SECOND VIS </p> -->
+                        <!-- <div class="text-left">
+                            <p><b>1.</b> Looking at the 2023 line, which month shows the most unusual or unexpected
+                                change compared to the pattern of the months right before and after it?</p>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="A1" class="mr-2 ">
+                                March
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="B1" class="mr-2 ">
+                                July
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="C1" class="mr-2 ">
+                                September
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="D1" class="mr-2">
+                                December
+                            </label>
+                        </div>
+                        <div class="text-left mt-5">
+                            <p><b>2.</b> Looking only at the 2023 line, what is the approximate range between its lowest
+                                and highest monthly values over the course of the year?</p>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="A2" class="mr-2 ">
+                                About 0.2°C
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="B2" class="mr-2 ">
+                                About 0.6°C
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="C2" class="mr-2 ">
+                                About 1.0°C
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="D2" class="mr-2">
+                                About 1.6°C
+                            </label>
+                        </div>
+                        <div class="text-left mt-10">
+                            <p><b>3.</b> If you had to summarize this chart in one sentence, what would you say?</p>
+                            <textarea id="message" rows="1" v-model="visMessage"
                                 class="mt-2 required rounded-lg bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body"
                                 placeholder="Write your thoughts here..."></textarea>
-                        </div>
+                        </div> -->
+                        <!-- <p> HERE FINISHES THE SECOND VIS </p> -->
 
-                        <div class="mt-10">
+
+
+
+                        <!-- <p> THESE ARE FOR THE THIRD VIS </p> -->
+                        <!-- <div class="text-left">
+                            <p><b>1.</b> Which of the following best describes the dominant climate risk pattern across
+                                the Southwest region of the U.S.?</p>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="A1" class="mr-2 ">
+                                Mostly water stress risk, with pockets of wildfire risk
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="B1" class="mr-2 ">
+                                Mostly hurricanes risk, with pockets of extreme rainfall
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="C1" class="mr-2 ">
+                                Mostly water stress risk, with pockets of sea level rise risk
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q1" value="D1" class="mr-2">
+                                Mostly extreme rainfall risk, with pockets of wildfire risk
+                            </label>
+                        </div>
+                        <div class="text-left mt-5">
+                            <p><b>2.</b> Looking at the map's overall color patterns, which of the following risk
+                                categories appears in the fewest, most scattered, isolated patches rather than large
+                                connected areas?</p>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="A2" class="mr-2 ">
+                                Water stress risk
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="B2" class="mr-2 ">
+                                Extreme heat risk
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="C2" class="mr-2 ">
+                                Sea level rise risk
+                            </label>
+                            <label class="flex items-center dark:text-white mt-2">
+                                <input type="radio" name="q2" value="D2" class="mr-2">
+                                Extreme rainfall risk
+                            </label>
+                        </div>
+                        <div class="text-left mt-10">
+                            <p><b>3.</b> If you had to summarize this chart in one sentence, what would you say?</p>
+                            <textarea id="message" rows="1" v-model="visMessage"
+                                class="mt-2 required rounded-lg bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body"
+                                placeholder="Write your thoughts here..."></textarea>
+                        </div> -->
+                        <!-- <p> HERE FINISHES THE THIRD VIS </p> -->
+
+
+
+
+                        <div class="mt-5 mb-5">
                             <button @click="nextStep" type="submit"
                                 class="bg-sky-900 hover:bg-sky-800 text-white px-4 py-2 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Next</button>
                         </div>
